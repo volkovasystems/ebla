0639292bb55ca93495abf736830eb3edfe56aaf0
Richeve Bebedor <richeve.bebedor@gmail.com>
2013-11-13 13:45:10 +0800

    Added ebla procedure. Added node_modules sub module.

---
+++ ebla.js
@@ -0,0 +1,18 @@
+var fs = require( "fs" );
+var async = require( "async" );
+
+var readNodeModulesDirectory = function readNodeModulesDirectory( ){
+
+};
+
+var readPackageConfiguration = function readPackageConfiguration( ){
+
+};
+
+var interpolateExistingModules = function interpolateExistingModules( ){
+
+};
+
+var boot = function boot( ){
+
+};
