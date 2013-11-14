3962477ebb1535c9e9dea04e6d17f2325c840673
Richeve Bebedor <richeve.bebedor@gmail.com>
2013-11-14 13:39:39 +0800

    Update ebla procedure.

--- ebla.js
+++ ebla.js
@@ -4,10 +4,29 @@ var async = require( "async" );
 var _ = require( "underscore" );
 var optimist = require( "optimist" );
 
-var readNodeModulesDirectory = function readNodeModulesDirectory( callback ){
+var readPackageConfiguration = function readPackageConfiguration( callback ){
+	var packageConfigurationPath = "./library-node/package.json";
+	fs.readFile( packageConfigurationPath,
+		function( error, configuration ){
+			if( error ){
+				console.log( error );
+				callback( error );
+				return;
+			}
+			var dependencyList = JSON.parse( configuration ).dependencies;
+			callback( null, dependencyList );
+		} );
+};
+
+var readNodeModulesDirectory = function readNodeModulesDirectory( dependencyList, callback ){
 	var nodeModuleDirectory = "./library-node/node_modules";
-	fs.readdir( "./library-node/node_modules",
+	fs.readdir( nodeModuleDirectory,
 		function( error, fileList ){
+			if( error ){
+				console.log( error );
+				callback( error );
+				return;
+			}
 			async.map( fileList,
 				function( fileName, callback ){
 					var filePath = nodeModuleDirectory + fileName;
@@ -38,17 +57,50 @@ var readNodeModulesDirectory = function readNodeModulesDirectory( callback ){
 						.each( function( directoryData ){
 							moduleList[ directoryData.name ] = directoryData.path
 						} );
-					callback( error, moduleList );
+					callback( error, moduleList, dependencyList );
 				} );
 		} );
 };
 
-var readPackageConfiguration = function readPackageConfiguration( ){
+var interpolateExistingModules = function interpolateExistingModules( moduleList, dependencyList, callback ){
+	for( var dependency in dependencyList ){
+		if( !( dependency in moduleList ) ){
+			callback( null, false );
+			return;
+		}
+	}
+	callback( null, true );
+};
+
+var checkModules = function checkModules( callback ){
+	async.waterfall( [
+			readPackageConfiguration,
+			readNodeModulesDirectory,
+			interpolateExistingModules
+		],
+		function( error, isComplete ){
+			if( error ){
+				console.log( error );
+				return;
+			}
+			if( isComplete ){
+				updateDependencies( );
+			}else{
+				completeDependencies( );
+			}
+		} );
+};
+
+var updateDependencies = function updateDependencies( ){
 
 };
 
-var interpolateExistingModules = function interpolateExistingModules( ){
+var completeDependencies = function completeDependencies( ){
+	var task = childprocess.exec( "cd library-node && npm install" );
+	task.on( "close",
+		function( ){
 
+		} );
 };
 
 var addModule = function addModule( moduleName ){
@@ -56,5 +108,9 @@ var addModule = function addModule( moduleName ){
 };
 
 var boot = function boot( ){
+	async.waterfall( [
+		],
+		function( error, result ){
 
+		} );
 };
